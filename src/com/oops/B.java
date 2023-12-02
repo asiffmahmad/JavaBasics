4aa75589b2ec767a75872de1b5cc930666dafca8
asiff <asiffmahmad9@gmail.com>
2023-12-02 19:09:24 +0530

    add a example for inheretence

---
+++ src/com/oops/B.java
@@ -0,0 +1,10 @@
+package com.oops;
+
+public class B extends A {
+
+	public String messageB() {
+		// TODO Auto-generated method stub
+		return "This is class B";
+	}
+
+}
--- src/com/oops/OOPSMain.java
+++ src/com/oops/OOPSMain.java
@@ -1,7 +1,13 @@
 package com.oops;
 
 public class OOPSMain {
-	private void mian() {
-		// TODO Auto-generated method stub
+
+	public static void main(String[] args) {
+		B b = new B();
+		C c = new C();
+		System.out.println("Class>>" + b.messageA());
+		System.out.println("Class>>" + b.messageB());
+		System.out.println("Class>>" + c.messageA());
+		System.out.println("Class>>" + c.messageB());
 	}
 }
